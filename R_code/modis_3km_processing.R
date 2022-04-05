61d53118a069fc360993351e09912e8315bc0c74
Brendan Turley <crabtails@gmail.com>
2022-04-05 10:09:40 -0500

    raster reprojeciton into UTM

--- R_code/modis_3km_processing.R
+++ R_code/modis_3km_processing.R
@@ -10,12 +10,19 @@ world <- readOGR('GSHHS_h_L1.shp')
 world <- crop(world, extent(-87, -79, 24, 31))
 w1 <- raster(ncol=400, nrow=400)
 extent(w1) <- extent(world)
-world_r <- rasterize(world)
 world_r <- rasterize(world,w1)
 world_r@data@values[which(world_r@data@values>1)] <- 1
 plot(world_r)
-world_r2 <- projectRaster(world_r,crs='+proj=utm +zone=17 +datum=NAD83 +units=km')
+### this reprojection does not work properly
+world_r2 <- projectRaster(world_r,crs='+proj=utm +zone=17 +datum=WGS84 +units=km +no_defs +ellps=WGS84 +towgs84=0,0,0')
 plot(world_r2)
+### this one does
+wr <- rast(world_r)
+plot(wr)
+wr2 <- project(wr,'+proj=utm +zone=17 +datum=WGS84 +units=km +no_defs +ellps=WGS84 +towgs84=0,0,0')
+plot(wr2)
+writeRaster(wr2, "output.tif", overwrite=TRUE)
+
 
 ### reference date and julian days
 yr <- 2021
