c84005c76f830e9a32b0ac227b451c593aeca6f0
Brendan Turley <crabtails@gmail.com>
2022-09-16 14:08:14 -0500

    hide appkey

--- R_code/nasa_download.R
+++ R_code/nasa_download.R
@@ -1,6 +1,3 @@
-# Issue: ---
-# Author: Catalino Cuadrado (catalino.cuadrado@nasa.gov)
-# Date: 12-19-2018
 
 library(ncdf4)
 library(httr)
@@ -9,16 +6,15 @@ library(httr)
 ### need unique appkey; keep this a secret!
 setwd('/Users/Brendan/Documents/nasa/cookies')
 appkey <- readLines('appkey.txt')
-# appkey <- '5fc9f83939bd1c6f91e9c6b4ab7b9fbfec56d97c'
 
 #Set up your ~/.netrc file as listed here: https://wiki.earthdata.nasa.gov/display/EL/How+To+Access+Data+With+cURL+And+Wget
 netrc_path <- "/Users/Brendan/Documents/nasa/cookies/cookie.netrc"
 cookie_path <- "/Users/Brendan/Documents/nasa/cookies/cookie.urs_cookies"
 
-
-### 1) httr method
+### example file to download
 url <- 'https://oceandata.sci.gsfc.nasa.gov/ob/getfile/AQUA_MODIS.20100109T000001.L2.OC.nc'
 
+### 1) httr method; based upon code here: https://wiki.earthdata.nasa.gov/display/EL/How+to+access+data+with+R
 downloaded_file_path <- "/Users/Brendan/Documents/nasa/download_test/test/test1.nc4"
 set_config(config(followlocation=1,
                   netrc=1,
@@ -34,6 +30,9 @@ t1.1 <- system.time(
                        overwrite = TRUE))
 )
 
+
+
+### both the curl and wget method are based upon info found here: https://oceancolor.gsfc.nasa.gov/data/download_methods/
 
 ### 2) curl method
 downloaded_file_path <- "/Users/Brendan/Documents/nasa/download_test/test/test2.nc4"
