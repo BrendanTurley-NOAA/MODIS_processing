59d6ffe5d0d0b5ab31d549e8f24786a6b85b8c7a
Brendan Turley <crabtails@gmail.com>
2022-09-27 16:02:51 -0500

    gpt processing pipeline testing / update

--- code/MODIS_gpt_pipeline_test.R
+++ code/MODIS_gpt_pipeline_test.R
@@ -11,33 +11,51 @@ setwd('~/Desktop/professional/projects/Postdoc_FL/hab_index/gpt_process')
 ### set files for GPT processing
 ### Both graph and parameter files where created using the SeaDAS cookbook
 ### https://seadas.gsfc.nasa.gov/help-8.1.0/GptCookbook/gptCookbook.html
-graph_file <- 'reproject_test5.xml'
+graph_file <- 'reproject_test7.xml'
 par_file <-  'ReprojectEx01.par'
 in_file <- 'A2005217182500.L2_LAC_OC.nc'
-out_file <- 'A2005217182500.L2_LAC_OC_reproject.nc'
+out_file <- 'A2005217182500.L2_LAC_OC_reproject5.nc'
 ### create command with appropriate files
-cmd <- paste('/Applications/snap/bin/gpt ',graph_file, ' -p ',par_file ,' -Pifile=',in_file,' -Pofile=',out_file,sep='')
+cmd <- paste0('/Applications/snap/bin/gpt ',graph_file, ' -p ',par_file ,' -Pifile=',in_file,' -Pofile=',out_file)
 ### run command
 t1 <- system.time(
   system(cmd) # calls command in Mac Terminal
 )
 
-
 ### open newly created files ans plot to see results
 setwd('~/Desktop/professional/projects/Postdoc_FL/hab_index/gpt_process')
 
-reproj1 <- nc_open('A2005217182500.L2_LAC_OC_reproject.nc')
+reproj1 <- nc_open('A2005217182500.L2_LAC_OC_reproject5.nc')
 attributes(reproj1$var)
+flags <- ncvar_get(reproj1, 'l2_masks')
+flags <- flags[,ncol(flags):1] # flip the matrix
+flags[which(flags==0)] <- NA
+flags[which(flags=='NaN')] <- NA
 chl_1 <- ncvar_get(reproj1, 'chlor_a')
+chl_1 <- chl_1[,ncol(chl_1):1] # flip the matrix
 lat <- ncvar_get(reproj1, 'lat')
 lat <- rev(lat) ### backwards for some reason, plotting hates that
 lon <- ncvar_get(reproj1, 'lon')
 nc_close(reproj1)
 
 
+png('test_flag_0.png',width=10,height=10,units='in',res=300)
 imagePlot(lon,
           lat,
-          log(chl_1[,ncol(chl_1):1],base=10),
+          log(chl_1,base=10),
           asp=1)
 map('usa',add=T)
 grid()
+dev.off()
+
+
+chl_m <- chl_1
+chl_m[which(!is.na(flags))] <- NA
+png('test_flag_1.png',width=10,height=10,units='in',res=300)
+imagePlot(lon,
+          lat,
+          log(chl_m,base=10),
+          asp=1)
+map('usa',add=T)
+grid()
+dev.off()
