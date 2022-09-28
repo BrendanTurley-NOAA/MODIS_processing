a0584f7544bf82e32c32a2cf9055f34eb6e5ff28
Brendan Turley <crabtails@gmail.com>
2022-09-28 13:14:40 -0500

    add names of wavelengths

--- code/solar_irradiance.R
+++ code/solar_irradiance.R
@@ -6,6 +6,7 @@
 # *F0_nnn in our use already includes pi
 
 data <- nc_open('A2005217182500.L2_LAC_OC.nc')
+wvlth <- substr(attributes(data$var)$names[grep('rrs',attributes(data$var)$names,ignore.case = T)],18,24)
 bands <- attributes(data$var)$names[grep('rrs',attributes(data$var)$names,ignore.case = T)]
 f0 <- rep(NA,10)
 for(i in 1:10){
@@ -15,3 +16,4 @@ for(i in 1:10){
   f0[i] <- tmp$value
 }
 
+names(f0) <- wvlth
