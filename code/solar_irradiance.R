621e0097aa4738420be57d532f08c624643b43e8
Brendan Turley <crabtails@gmail.com>
2022-09-30 13:21:59 -0500

    solar irradiance

--- code/solar_irradiance.R
+++ code/solar_irradiance.R
@@ -1,10 +1,17 @@
+library(ncdf4)
+
 ### get solar_irradiance values fron netcdf file
-# solar_irradiance units: W/m^2/um/sr
+# solar_irradiance units: W m^-2 um^-1
+# nlw units: W m^-2 um^-1 sr^-1
+# coastWatch nLw units: mW cm^-2 um^-1 sr^-1
+# RRS units: sr^-1
 # see this reference: https://oceancolor.gsfc.nasa.gov/docs/rsr/rsr_tables/
 # The relationship between Rrs and nLw is:
 # Rrs_nnn = nLw_nnn / F0_nnn* where nnn is the wavelength/band and F0 is the solar_irradiance attribute value for each band.
 # *F0_nnn in our use already includes pi
 
+### set working directory
+setwd('~/Desktop/professional/projects/Postdoc_FL/hab_index/gpt_process')
 data <- nc_open('A2005217182500.L2_LAC_OC.nc')
 wvlth <- substr(attributes(data$var)$names[grep('rrs',attributes(data$var)$names,ignore.case = T)],18,24)
 bands <- attributes(data$var)$names[grep('rrs',attributes(data$var)$names,ignore.case = T)]
@@ -17,3 +24,10 @@ for(i in 1:10){
 }
 
 names(f0) <- wvlth
+
+### convert F0 from W m^-2 um^-1 (given by NASA) to mW cm^-2 um^-1 (used by Coastwatch and Dr. Hu)
+# watt to mW => 1000*W
+# m^-2 to cm^-1 => 1/10000*m^-2
+convert <- 1000/10000
+
+f0*convert
