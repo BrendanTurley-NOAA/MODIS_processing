4b3ecbbdcc887a78233e5fa51576e0f7cc5546b0
Brendan Turley <crabtails@gmail.com>
2022-11-03 16:19:53 -0500

    memory usage

--- code/lowres_index_dev.R
+++ code/lowres_index_dev.R
@@ -1,3 +1,6 @@
+library(fields)
+library(ncdf4)
+
 ### 1) download daily 4km Aqua data
 # a) what bounding box
 # (-87.5 30.7, -81, 24.2)
@@ -41,8 +44,6 @@ last <- ifelse(leap_year(paste0(yr,'-01-01')),366,365)
 
 dates <- data.frame(date=ymd(seq(as.Date(paste0(yr,'-01-01')),as.Date(paste0(yr,'-12-31')),'day')),
                     yday=yday(ymd(seq(as.Date(paste0(yr,'-01-01')),as.Date(paste0(yr,'-12-31')),'day'))))
-start <- dates$yday[which(day(dates$date)==1)]
-stop <- c(dates$yday[which(day(dates$date)==1)-1],last)
 parms <- c('CHL_chlor_a','FLH_nflh','RRS_Rrs_443','RRS_Rrs_488','RRS_Rrs_531','RRS_Rrs_547','RRS_Rrs_555','RRS_Rrs_667','RRS_Rrs_678')
 parm <- substr(parms,5,11)
 
@@ -60,25 +61,42 @@ lon2 <- ncvar_get(modis, 'lon',start=lon_start,count=lon_count)
 lat2 <- ncvar_get(modis, 'lat',start=lat_start,count=lat_count)
 
 data_yday <- array(NA,c(nrow(dates),9,length(lon2),length(lat2)))
-for(i in 1:nrow(dates)){
-  for(j in 1:lenth[parms]){
-    url <- paste0('http://oceandata.sci.gsfc.nasa.gov/opendap/MODISA/L3SMI/',
-                  yr,
-                  '/',
-                  sprintf("%03d",dates$yday[i]),
-                  '/A',
-                  yr,
-                  sprintf("%03d",dates$yday[i]),
-                  '.L3m_DAY_',
-                  parms[j],
-                  '_4km.nc')
-    modis <- nc_open(url)
-    data <- ncvar_get(modis, parm[i],start=c(lon_start,lat_start),count=c(lon_count,lat_count))
-    nc_close(modis)
-    
-    data_yday[i,j,,] <- data
+data_yday <- array(NA,c(1,9,length(lon2),length(lat2)))
+t1 <- system.time(
+  for(i in 1:nrow(dates)){
+    for(j in 1:length(parms)){
+      url <- paste0('http://oceandata.sci.gsfc.nasa.gov/opendap/MODISA/L3SMI/',
+                    yr,
+                    '/',
+                    sprintf("%03d",dates$yday[i]),
+                    '/A',
+                    yr,
+                    sprintf("%03d",dates$yday[i]),
+                    '.L3m_DAY_',
+                    parms[j],
+                    '_4km.nc')
+      modis <- nc_open(url)
+      data <- ncvar_get(modis, parm[j],start=c(lon_start,lat_start),count=c(lon_count,lat_count))
+      nc_close(modis)
+      
+      data_yday[i,j,,] <- data
+    }
   }
+)
+
+par(mfrow=c(2,2))
+for(i in 1:9){
+  imagePlot(lon2,rev(lat2),
+        log10(data_yday[1,i,,(dim(data_yday)[4]:1)]),
+        asp=1)
+  mtext(parm[i])
 }
+
+# how much storage will this take?
+data_yday <- array(1000000,c(1,9,length(lon2),length(lat2)))
+size <- object.size(data_yday)
+ # 1 day ~ 1.8 mb; 
+format(size*365*22,units='GB')
 
 
 # d) what derived parameters post-processing
