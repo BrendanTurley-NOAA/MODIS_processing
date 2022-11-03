51ea5aadbf8320df77a4ddb69da13c54325c0244
Brendan Turley <crabtails@gmail.com>
2022-11-03 13:52:32 -0500

    update

---
+++ code/lowres_index_dev.R
@@ -0,0 +1,13 @@
+### 1) download daily 4km Aqua data
+
+# 1a) which parameters
+
+# b) what bounding box
+
+# c) what derived parameters
+
+
+### 2) match up to HAB monitioring data
+
+
+### 3) create model
