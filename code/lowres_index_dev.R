be2661e1840d5a1cd50dda7990731ed7e9cf3679
Brendan Turley <crabtails@gmail.com>
2022-11-06 22:20:40 -0600

    loop over years

--- code/lowres_index_dev.R
+++ code/lowres_index_dev.R
@@ -93,84 +93,91 @@ lat_count <- length(lat_start:lat_stop)
 lon2 <- ncvar_get(modis1, 'lon',start=lon_start,count=lon_count)
 lat2 <- ncvar_get(modis1, 'lat',start=lat_start,count=lat_count)
 
-### reference date and julian days
-yr <- 2021 # 2003:2021
-dates <- data.frame(date=ymd(seq(as.Date(paste0(yr,'-01-01')),as.Date(paste0(yr,'-12-31')),'day')),
-                    yday=yday(ymd(seq(as.Date(paste0(yr,'-01-01')),as.Date(paste0(yr,'-12-31')),'day'))))
-### create netcdf file
-dimlon <- ncdim_def('Lon','degreesE',lon2)
-dimlat <- ncdim_def('Lat','degreesN',lat2)
-dates1 <- as.POSIXct(paste0(dates$date,00:00),tz='GMT')
-dates2 <- as.numeric(dates1)/86400
-# as.Date(dates2[1],origin='1970-01-01')
-dimtime <- ncdim_def('Time','days since 1970-01-01',dates2)
-chlor_a <- ncvar_def('chlor_a','mg m^-3',list(dimlon,dimlat,dimtime),-32767,"Chlorophyll Concentration, OCI Algorithm",prec='double',compression=5) # prec='float may be smaller'
-nflh <- ncvar_def('nflh','W m^-2 um^-1 sr^-1',list(dimlon,dimlat,dimtime),-32767,"Normalized Fluorescence Line Height",prec='double',compression=5) # prec='float may be smaller'
-rrs_443 <- ncvar_def('Rrs_443','sr^-1',list(dimlon,dimlat,dimtime),-32767,"Remote sensing reflectance at 443 nm",prec='double',compression=5) # prec='float may be smaller'
-rrs_488 <- ncvar_def('Rrs_488','sr^-1',list(dimlon,dimlat,dimtime),-32767,"Remote sensing reflectance at 488 nm",prec='double',compression=5) # prec='float may be smaller'
-rrs_531 <- ncvar_def('Rrs_531','sr^-1',list(dimlon,dimlat,dimtime),-32767,"Remote sensing reflectance at 531 nm",prec='double',compression=5) # prec='float may be smaller'
-rrs_547 <- ncvar_def('Rrs_547','sr^-1',list(dimlon,dimlat,dimtime),-32767,"Remote sensing reflectance at 547 nm",prec='double',compression=5) # prec='float may be smaller'
-rrs_555 <- ncvar_def('Rrs_555','sr^-1',list(dimlon,dimlat,dimtime),-32767,"Remote sensing reflectance at 555 nm",prec='double',compression=5) # prec='float may be smaller'
-rrs_667 <- ncvar_def('Rrs_667','sr^-1',list(dimlon,dimlat,dimtime),-32767,"Remote sensing reflectance at 667 nm",prec='double',compression=5) # prec='float may be smaller'
-rrs_678 <- ncvar_def('Rrs_678','sr^-1',list(dimlon,dimlat,dimtime),-32767,"Remote sensing reflectance at 678 nm",prec='double',compression=5) # prec='float may be smaller'
-# modis_tmp <- nc_create('modis_tmp.nc',list(chlor_a,nflh,rrs_443,rrs_488,rrs_531,rrs_547,rrs_555,rrs_667,rrs_678))
-modis_tmp <- nc_create(paste0('modisa_daily_',yr,'.nc'),list(chlor_a,nflh,rrs_443,rrs_488,rrs_531,rrs_547,rrs_555,rrs_667,rrs_678))
-nc.copy.atts(modis1,0,modis_tmp,0,names(atts)[c(1,6,8,9,11:14,18:26,29:34,36:44,46:48,51,53,55,57:64)]) # not tested
-ncatt_put(modis_tmp,0,"time_coverage_start",paste(dates1[1]))
-ncatt_put(modis_tmp,0,"time_coverage_end",paste(dates1[length(dates1)]))
-ncatt_put(modis_tmp,0,"modified_by",'Brendan Turley')
-
-data_yday <- array(NA,c(9,
-                        length(lon2),
-                        length(lat2),
-                        nrow(dates)))
-pb <- txtProgressBar(min = 0, max = nrow(dates), style = 3)
-t1 <- system.time(
-  for(i in 1:nrow(dates)){
-    for(j in 1:length(parms)){
-      url <- paste0('http://oceandata.sci.gsfc.nasa.gov/opendap/MODISA/L3SMI/',
-                    yr,
-                    '/',
-                    sprintf("%03d",dates$yday[i]),
-                    '/A',
-                    yr,
-                    sprintf("%03d",dates$yday[i]),
-                    '.L3m_DAY_',
-                    parms[j],
-                    '_4km.nc')
-      modis <- nc_open(url)
-      data <- ncvar_get(modis,parm[j],start=c(lon_start,lat_start),count=c(lon_count,lat_count))
-      if(i==1){
-        nc.copy.atts(modis,parm[j],modis_tmp,parm[j],c('name','units','dim','_FillValue','longname','prec')) # not tested
+times1 <- rep(NA,length(2003:2021))
+for(yr in 2003:2021){
+  ### reference date and julian days
+  # yr <- 2021 # 2003:2021
+  dates <- data.frame(date=ymd(seq(as.Date(paste0(yr,'-01-01')),as.Date(paste0(yr,'-12-31')),'day')),
+                      yday=yday(ymd(seq(as.Date(paste0(yr,'-01-01')),as.Date(paste0(yr,'-12-31')),'day'))))
+  ### create netcdf file
+  dimlon <- ncdim_def('Lon','degreesE',lon2)
+  dimlat <- ncdim_def('Lat','degreesN',lat2)
+  dates1 <- as.POSIXct(paste0(dates$date,00:00),tz='GMT')
+  dates2 <- as.numeric(dates1)/86400
+  # as.Date(dates2[1],origin='1970-01-01')
+  dimtime <- ncdim_def('Time','days since 1970-01-01',dates2)
+  chlor_a <- ncvar_def('chlor_a','mg m^-3',list(dimlon,dimlat,dimtime),-32767,"Chlorophyll Concentration, OCI Algorithm",prec='double',compression=5) # prec='float may be smaller'
+  nflh <- ncvar_def('nflh','W m^-2 um^-1 sr^-1',list(dimlon,dimlat,dimtime),-32767,"Normalized Fluorescence Line Height",prec='double',compression=5) # prec='float may be smaller'
+  rrs_443 <- ncvar_def('Rrs_443','sr^-1',list(dimlon,dimlat,dimtime),-32767,"Remote sensing reflectance at 443 nm",prec='double',compression=5) # prec='float may be smaller'
+  rrs_488 <- ncvar_def('Rrs_488','sr^-1',list(dimlon,dimlat,dimtime),-32767,"Remote sensing reflectance at 488 nm",prec='double',compression=5) # prec='float may be smaller'
+  rrs_531 <- ncvar_def('Rrs_531','sr^-1',list(dimlon,dimlat,dimtime),-32767,"Remote sensing reflectance at 531 nm",prec='double',compression=5) # prec='float may be smaller'
+  rrs_547 <- ncvar_def('Rrs_547','sr^-1',list(dimlon,dimlat,dimtime),-32767,"Remote sensing reflectance at 547 nm",prec='double',compression=5) # prec='float may be smaller'
+  rrs_555 <- ncvar_def('Rrs_555','sr^-1',list(dimlon,dimlat,dimtime),-32767,"Remote sensing reflectance at 555 nm",prec='double',compression=5) # prec='float may be smaller'
+  rrs_667 <- ncvar_def('Rrs_667','sr^-1',list(dimlon,dimlat,dimtime),-32767,"Remote sensing reflectance at 667 nm",prec='double',compression=5) # prec='float may be smaller'
+  rrs_678 <- ncvar_def('Rrs_678','sr^-1',list(dimlon,dimlat,dimtime),-32767,"Remote sensing reflectance at 678 nm",prec='double',compression=5) # prec='float may be smaller'
+  # modis_tmp <- nc_create('modis_tmp.nc',list(chlor_a,nflh,rrs_443,rrs_488,rrs_531,rrs_547,rrs_555,rrs_667,rrs_678))
+  modis_tmp <- nc_create(paste0('modisa_daily_',yr,'.nc'),list(chlor_a,nflh,rrs_443,rrs_488,rrs_531,rrs_547,rrs_555,rrs_667,rrs_678))
+  nc.copy.atts(modis1,0,modis_tmp,0,names(atts)[c(1,6,8,9,11:14,18:26,29:34,36:44,46:48,51,53,55,57:64)]) # not tested
+  ncatt_put(modis_tmp,0,"time_coverage_start",paste(dates1[1]))
+  ncatt_put(modis_tmp,0,"time_coverage_end",paste(dates1[length(dates1)]))
+  ncatt_put(modis_tmp,0,"modified_by",'Brendan Turley')
+  
+  data_yday <- array(NA,c(9,
+                          length(lon2),
+                          length(lat2),
+                          nrow(dates)))
+  pb <- txtProgressBar(min = 0, max = nrow(dates), style = 3)
+  t1 <- system.time(
+    for(i in 1:nrow(dates)){
+      for(j in 1:length(parms)){
+        url <- paste0('http://oceandata.sci.gsfc.nasa.gov/opendap/MODISA/L3SMI/',
+                      yr,
+                      '/',
+                      sprintf("%03d",dates$yday[i]),
+                      '/A',
+                      yr,
+                      sprintf("%03d",dates$yday[i]),
+                      '.L3m_DAY_',
+                      parms[j],
+                      '_4km.nc')
+        modis <- try(nc_open(url))
+        if(class(modis)!='try-error'){
+          data <- ncvar_get(modis,parm[j],start=c(lon_start,lat_start),count=c(lon_count,lat_count))
+          if(i==1){
+            nc.copy.atts(modis,parm[j],modis_tmp,parm[j],c('name','units','dim','_FillValue','longname','prec')) # not tested
+          }
+          nc_close(modis)
+          data_yday[j,,,i] <- data
+        }
+        rm(modis,data,url)
       }
-      nc_close(modis)
-      data_yday[j,,,i] <- data
-      rm(modis,data,url)
+      setTxtProgressBar(pb, i)
     }
-    setTxtProgressBar(pb, i)
-  }
-)
-t1
-# "2022-11-04 11:26:35 CDT"
-# user   system  elapsed 
-# 134.101   48.079 4389.390 
-# data_yday <- array(1000000,c(9,length(lon2),length(lat2),365))
-
-### fill netcdf file
-ncvar_put(modis_tmp,chlor_a,data_yday[1,,,])
-ncvar_put(modis_tmp,nflh,data_yday[2,,,])
-ncvar_put(modis_tmp,rrs_443,data_yday[3,,,])
-ncvar_put(modis_tmp,rrs_488,data_yday[4,,,])
-ncvar_put(modis_tmp,rrs_531,data_yday[5,,,])
-ncvar_put(modis_tmp,rrs_547,data_yday[6,,,])
-ncvar_put(modis_tmp,rrs_555,data_yday[7,,,])
-ncvar_put(modis_tmp,rrs_667,data_yday[8,,,])
-ncvar_put(modis_tmp,rrs_678,data_yday[9,,,])
-ncatt_put(modis_tmp,0,"last_Modified",paste(Sys.time()))
-nc_close(modis_tmp)
-
-# saveRDS(data_yday,paste0('modisa_daily_',yr,'.rds')) # netcdf is smaller and contains metadata
-data_yday <- readRDS('modisa_daily_2021.rds')
+  )
+  print(t1)
+  times1[yr-2002] <- t1[3]
+  # "2022-11-04 11:26:35 CDT"
+  # user   system  elapsed 
+  # 134.101   48.079 4389.390 
+  # data_yday <- array(1000000,c(9,length(lon2),length(lat2),365))
+  
+  ### fill netcdf file
+  ncvar_put(modis_tmp,chlor_a,data_yday[1,,,])
+  ncvar_put(modis_tmp,nflh,data_yday[2,,,])
+  ncvar_put(modis_tmp,rrs_443,data_yday[3,,,])
+  ncvar_put(modis_tmp,rrs_488,data_yday[4,,,])
+  ncvar_put(modis_tmp,rrs_531,data_yday[5,,,])
+  ncvar_put(modis_tmp,rrs_547,data_yday[6,,,])
+  ncvar_put(modis_tmp,rrs_555,data_yday[7,,,])
+  ncvar_put(modis_tmp,rrs_667,data_yday[8,,,])
+  ncvar_put(modis_tmp,rrs_678,data_yday[9,,,])
+  ncatt_put(modis_tmp,0,"last_Modified",paste(Sys.time()))
+  nc_close(modis_tmp)
+  
+  saveRDS(data_yday,paste0('modisa_daily_',yr,'.rds')) # netcdf is smaller and contains metadata
+  # data_yday <- readRDS('modisa_daily_2021.rds')
+}
+cat('total time:',sum(times1), 'sec')
 
 par(mfrow=c(2,2))
 for(i in 1:9){
